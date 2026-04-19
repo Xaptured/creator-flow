08cea36b48290b6d2f9e0883902105012fa8530d
Jack <44050726+Xaptured@users.noreply.github.com>
2026-04-19 20:35:04 +0530

    CF-26 feat: initialize auth-service and media-service with Spring Boo… (#22)

--- backend/media-service/src/main/java/com/creatorflow/media_service/MediaServiceApplication.java
+++ backend/media-service/src/main/java/com/creatorflow/media_service/MediaServiceApplication.java
@@ -1,9 +1,12 @@
 package com.creatorflow.media_service;
 
+import com.creatorflow.media_service.configuration.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsConfig.class)
 public class MediaServiceApplication {
 
 	public static void main(String[] args) {
