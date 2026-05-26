6f38d8cdb7aa235731a589c8c516be49f2b1add8
Jack <jk19011999@gmail.com>
2026-05-27 01:38:17 +0530

    CF-92 feat: Implemented ai-service basic stuff

--- ai-service/src/analytics/analytics.module.ts
+++ ai-service/src/analytics/analytics.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { AnalyticsController } from './analytics.controller.js';
 import { AnalyticsRepository } from './analytics.repository.js';
 import { AnalyticsService } from './analytics.service.js';
 
 @Module({
+  controllers: [AnalyticsController],
   providers: [AnalyticsRepository, AnalyticsService],
   exports: [AnalyticsService],
 })
